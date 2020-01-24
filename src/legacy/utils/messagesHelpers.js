6551f93404e586ce266157eabc8edd029c9368a4
Parrish <paarriisshh@yahoo.com.mx>
2020-01-23 22:58:36 -0600

    add split option to raw mesages

--- src/legacy/utils/messagesHelpers.js
+++ src/legacy/utils/messagesHelpers.js
@@ -1,17 +1,14 @@
-
 // relativo al envio de mensajes
-const responceTo = responceFn => toResponce => options =>
-  responceFn(options, toResponce)
+const responceTo = responceFn => toResponce => options => responceFn(options, toResponce)
 
-const rawMessage = responceTo(
-  (options, toResponce) => {
-    if (typeof options === 'object') {
-      let { text, embed } = options
-      setTimeout(() => toResponce.channel.send(text, { embed: embed }), 500)
-    } else setTimeout(() => toResponce.channel.send(options), 500)
-  })
+const rawMessage = responceTo((options, toResponce) => {
+  if (typeof options === 'object') {
+    const { text, embed } = options
+    setTimeout(() => toResponce.channel.send(text, { embed, split: true }), 500)
+  } else setTimeout(() => toResponce.channel.send(options), 500)
+})
 
-function makeCode (text) {
+function makeCode(text) {
   return `\`\`\`${text}\`\`\``
 }
 
