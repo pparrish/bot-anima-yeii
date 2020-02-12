563ab30e2041cf36d338ddfc933c8ac3832e4c03
Parrish <paarriisshh@yahoo.com.mx>
2020-02-12 13:17:36 -0600

    add lint staged

---
+++ lint-staged.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  // Lint js files
+  '*.js': filenames => {
+    return [
+      `npx jest --findRelatedTests ${filenames.join(' ')}`,
+      `npx eslint --fix ${filenames.join(' ')}`,
+      `git add ${filenames.join(' ')}`
+    ]
+  },
+  'src/**/!(*test|*steps).js': filenames => {
+    const lintDoc = filenames.map(file => {
+      if (file.endsWith('.config.js')) return ''
+      return `documentation lint ${file}`
+    })
+    return lintDoc
+  }
+}
