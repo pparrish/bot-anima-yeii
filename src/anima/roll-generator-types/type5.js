e92f92798710704625052d9e220d5d3e2ca2838f
Parrish <paarriisshh@yahoo.com.mx>
2020-03-06 23:36:13 -0600

    add type5 generator

---
+++ src/anima/roll-generator-types/type5.js
@@ -0,0 +1,7 @@
+export const name = 'type 5'
+export const rollRule = null
+export const type = 'points'
+export const need = 'points to generate'
+export const generator = points => {
+  return { points }
+}
