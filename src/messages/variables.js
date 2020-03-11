49a4828a8fcee61b894f5e20356e884f8253dfea
Parrish <paarriisshh@yahoo.com.mx>
2020-03-10 18:15:41 -0600

    The variables command separe characteristics, primary, secondary and another variables

--- src/messages/variables.js
+++ src/messages/variables.js
@@ -62,21 +62,143 @@ O puedes subir una ficha de anima entera con
   },
 }
 
+// Caracteristicas
+const characteristicNames = [
+  'fuerza',
+  'destreza',
+  'inteligencia',
+  'percepción',
+  'agilidad',
+  'poder',
+  'constitución',
+  'voluntad',
+]
+// Primarias
+const primaryNames = [
+  'ataque',
+  'parada',
+  'esquiva',
+  'mágica',
+  'psíquica',
+  'potencial',
+  'convocar',
+  'atadura',
+  'dominación',
+  'desconvocar',
+]
+// Secundarias
+const secondaryNames = [
+  'acrobacias',
+  'advertir',
+  'alquimia',
+  'animales',
+  'animismo',
+  'arte',
+  'atletismo',
+  'baile',
+  'buscar',
+  'caligrafíaritual',
+  'callejeo',
+  'cerrajería',
+  'ciencia',
+  'comercio',
+  'confección',
+  'confmarionetas',
+  'disfraz',
+  'estilo',
+  'etiqueta',
+  'forja',
+  'frialdad',
+  'herbolaria',
+  'historia',
+  'intimidar',
+  'ley',
+  'liderazgo',
+  'medicina',
+  'memorizar',
+  'montar',
+  'música',
+  'nadar',
+  'navegación',
+  'ocultarse',
+  'ocultismo',
+  'orfebrería',
+  'persuasión',
+  'pfuerza',
+  'pilotar',
+  'rastrear',
+  'resdolor',
+  'robo',
+  'runas',
+  'saltar',
+  'sigilo',
+  'tactica',
+  'tasación',
+  'tmanos',
+  'trampería',
+  'trepar',
+  'venenos',
+  'vmágica',
+]
 export const allVariables = {
   name: 'all variables',
   resolver: (
     { variables, selectedSheet },
     { channel }
   ) => {
-    const variablesStr = Object.entries(
-      variables
-    ).reduce(
-      (accStr, v) =>
-        `${accStr}**${v[0]}** = ${v[1]}\n`,
-      ''
+    const characteristics = {
+      have: false,
+      str:
+        '\\_\\_\\_\\_\n**Características\n==============================**',
+    }
+    const primary = {
+      have: false,
+      str:
+        '\\_\\_\\_\\_\n**Primarías\n==============================**',
+    }
+    const secondary = {
+      have: false,
+      str:
+        '\\_\\_\\_\\_\n**Secundarías\n==============================**',
+    }
+    const another = {
+      have: false,
+      str:
+        '\\_\\_\\_\\_\n**Variables\n==============================**',
+    }
+
+    // eslint-disable-next-line no-shadow
+    Object.entries(variables).map(variable => {
+      if (
+        characteristicNames.includes(variable[0])
+      ) {
+        characteristics.have = true
+        characteristics.str += `\n**${variable[0]}** = ${variable[1]}`
+        return false
+      }
+      if (primaryNames.includes(variable[0])) {
+        primary.have = true
+        primary.str += `\n**${variable[0]}** = ${variable[1]}`
+        return false
+      }
+      if (secondaryNames.includes(variable[0])) {
+        secondary.have = true
+        secondary.str += `\n**${variable[0]}** = ${variable[1]}`
+        return false
+      }
+      another.have = true
+      another.str += `\n**${variable[0]}** = ${variable[1]}`
+      return false
+    })
+
+    channel.send(
+      `Variables guardadas en **${selectedSheet}**
+${characteristics.have ? characteristics.str : ''}
+${primary.have ? primary.str : ''}
+${secondary.have ? secondary.str : ''}
+${another.have ? another.str : ''}
+`
     )
-    channel.send(`Variables guardadas en **${selectedSheet}**
-${variablesStr}`)
   },
 }
 
