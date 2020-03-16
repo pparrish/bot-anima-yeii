ffc982c9d5e5d02fd4ebfdf5b115c9877be1013e
Parrish <paarriisshh@yahoo.com.mx>
2020-03-16 00:46:20 -0600

    Add .pv command help messages

--- src/commands/help.js
+++ src/commands/help.js
@@ -170,6 +170,29 @@ Genera las tiradas correspondientes a Generación de tiradas"" (p: 10-11 core.)"
 \`\`\`
 Hace todas las tiradas pertinentes para crear un personaje para el juego de "Clásicos del Mazmorreo".
 `,
+  LIFE_POINTS: `Uso detallado del comando pv(puntos de vida).
+Asigna valor a los pv de vida de la ficha actual.
+\`\`\`
+.pv < = | + | - | % | +% | -% | número > <valor>
+\`\`\`
+La primera parte del comando es la operación, la cual determina la forma en la cual va a cambiar el valor de los pv.
+= : igual al valor.
++ : puntos de vida más el valor.
+- : puntos de vida menos el valor.
+% : el valor por ciento de los puntos de vida.
++% : puntos de vida menos el valor por ciento de los puntos de vida.
+-% : punros de vida más el valor por ciento de los puntos de vida.
+núnero : igual al numero dado mas el valor
+
+La segunda parte llamada valor es el numero con el cual vamos a operar.
+
+Ejemplos.
+\`.pv 75\` Tus pv son ahora 75
+\`.pv = 90 + 10\` Tus pv son ahora 100 (90+10)
+\`.pv +% 10\` Aumentar un 10% tus pv actuales
+\`.pv % 50\` Reducir a la mitad los pv.
+\`.pv - 10 + daño\` Suponiendo que tienes una variable llamada daño que valga 40, tus pv se van a reducir -50
+`,
 }
 
 export default (
@@ -223,6 +246,12 @@ export default (
     return messenger.send(
       'raw',
       { text: messages.DELETE_SHEET },
+      context
+    )
+  if (command === 'pv')
+    return messenger.send(
+      'raw',
+      { text: messages.LIFE_POINTS },
       context
     )
   if (command === 'otros')
