d1c70733731e01d1bf6f5fb17dc80ba8bd0c6d91
Parrish <paarriisshh@yahoo.com.mx>
2020-06-12 02:12:53 -0500

    fix(firebase):Now the credentials function return undefined properties

--- src/storage/FirebaseCrudWrapper.js
+++ src/storage/FirebaseCrudWrapper.js
@@ -3,45 +3,43 @@ import * as admin from 'firebase-admin'
 require('dotenv').config()
 
 const credentials = () => {
-  if (
-    process.env.FB_type &&
-    process.env.FB_project_id &&
-    process.env.FB_private_key_id &&
-    process.env.FB_private_key &&
-    process.env.FB_client_email &&
-    process.env.FB_client_id &&
-    process.env.FB_auth_uri &&
-    process.env.FB_token_uri &&
-    process.env.FB_auth_provider_x509_cert_url &&
-    process.env.FB_client_x509_cert_url
-  ) {
-    return {
-      type: process.env.FB_type,
-      project_id: process.env.FB_project_id,
-      private_key_id:
-        process.env.FB_private_key_id,
-      private_key: process.env.FB_private_key,
-      client_email: process.env.FB_client_email,
-      client_id: process.env.FB_client_id,
-      auth_uri: process.env.FB_auth_uri,
-      token_uri: process.env.FB_token_uri,
-      auth_provider_x509_cert_url:
-        process.env
-          .FB_auth_provider_x509_cert_url,
-      client_x509_cert_url:
-        process.env.FB_client_x509_cert_url,
-    }
+  return {
+    type: process.env.FB_type || undefined,
+    project_id:
+      process.env.FB_project_id || undefined,
+    private_key_id:
+      process.env.FB_private_key_id || undefined,
+    private_key:
+      process.env.FB_private_key || undefined,
+    client_email:
+      process.env.FB_client_email || undefined,
+    client_id:
+      process.env.FB_client_id || undefined,
+    auth_uri:
+      process.env.FB_auth_uri || undefined,
+    token_uri:
+      process.env.FB_token_uri || undefined,
+    auth_provider_x509_cert_url:
+      process.env
+        .FB_auth_provider_x509_cert_url ||
+      undefined,
+    client_x509_cert_url:
+      process.env.FB_client_x509_cert_url ||
+      undefined,
   }
-  return {}
 }
 
-admin.initializeApp({
-  credential: admin.credential.cert(
-    credentials()
-  ),
-  databaseURL:
-    'https://anima-yeii-83347.firebaseio.com',
-})
+try {
+  admin.initializeApp({
+    credential: admin.credential.cert(
+      credentials()
+    ),
+    databaseURL:
+      'https://anima-yeii-83347.firebaseio.com',
+  })
+} catch (e) {
+  console.log(e)
+}
 
 export default class FirebaseCrudWrapper {
   constructor() {
