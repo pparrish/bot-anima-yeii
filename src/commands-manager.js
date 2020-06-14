3036ea9482f59173e0d0d8940f385aba4e33838f
Parrish <paarriisshh@yahoo.com.mx>
2020-06-14 01:30:01 -0500

    refactor(commandManager): Move commands to command-manager

--- src/commands-manager.js
+++ src/commands-manager.js
@@ -1,6 +1,196 @@
 import CommandManager from './command-manager'
 import messagesManager from './messages-manager'
-import commands from './commands'
+import prefixedValues from './parsers/prefixedValues'
+import variablesParser from './parsers/variables'
+import calcParser from './parsers/calc'
+import rollResolver from './commands/roll'
+import AbilityDice from './dices/AbilityDice'
+import CharacteristicDice from './dices/CharacteristicDice'
+import variables from './commands/variables'
+import first from './parsers/first'
+import deleteVariable from './commands/deleteVariable'
+import rest from './parsers/rest'
+import issue from './commands/issue'
+import tables from './commands/tables'
+import help from './commands/help'
+import sheet from './commands/sheet'
+import deleteSheet from './commands/deleteSheet'
+import tldr from './commands/tldr'
+import rollGeneratorType from './commands/roll-generator-type'
+import createCharacter from './commands/create-character'
+import cdmCreateACharacter from './clasicos-del-mazmorreo/commands/create-character'
+import lifePoints from './commands/life-points'
+import testCommand from './commands/test-command'
+
+const parseOptions = {
+  name: 'options',
+  parser: prefixedValues('?'),
+  consume: true,
+}
+const parseVariables = {
+  name: 'variables',
+  parser: variablesParser,
+  consume: false,
+}
+const parseCalc = {
+  name: 'calc',
+  parser: calcParser,
+  consume: true,
+}
+
+const parseQuerry = {
+  name: 'querry',
+  parser: rest,
+  consume: true,
+}
+const commands = [
+  {
+    name: 't',
+    resolver: rollResolver(AbilityDice, 'roll'),
+    options: [
+      parseOptions,
+      parseVariables,
+      parseCalc,
+    ],
+  },
+  {
+    name: 'd',
+    resolver: rollResolver(
+      CharacteristicDice,
+      'ability roll'
+    ),
+    options: [
+      parseOptions,
+      parseVariables,
+      parseCalc,
+    ],
+  },
+  {
+    name: 'gv',
+    resolver: variables,
+    options: [
+      {
+        name: 'name',
+        parser: first,
+        consume: true,
+      },
+      {
+        name: 'value',
+        parser: first,
+        consume: true,
+      },
+    ],
+  },
+  {
+    name: 'bv',
+    resolver: deleteVariable,
+    options: [
+      {
+        name: 'name',
+        parser: first,
+        consume: true,
+      },
+    ],
+  },
+  {
+    name: 'issue###',
+    resolver: issue,
+    options: [
+      {
+        name: 'message',
+        parser: rest,
+        consume: true,
+      },
+    ],
+  },
+  {
+    name: 'tb',
+    resolver: tables,
+    options: [parseQuerry],
+  },
+  {
+    name: 'h',
+    resolver: help,
+    options: [
+      {
+        name: 'command',
+        parser: first,
+        consume: true,
+      },
+    ],
+  },
+  {
+    name: 'ficha',
+    resolver: sheet,
+    options: [parseOptions, parseQuerry],
+  },
+  {
+    name: 'bficha',
+    resolver: deleteSheet,
+    options: [parseQuerry],
+  },
+  {
+    name: 'tldr',
+    resolver: tldr,
+    options: [parseQuerry],
+  },
+  {
+    name: 'pv',
+    resolver: lifePoints,
+    options: [
+      {
+        name: 'mod',
+        parser: first,
+        consume: true,
+      },
+      {
+        name: 'points',
+        parser: rest,
+        consume: true,
+      },
+    ],
+  },
+  {
+    name: 'generar-tirada-tipo',
+    resolver: rollGeneratorType,
+    options: [
+      {
+        name: 'type',
+        parser: first,
+        consume: true,
+      },
+    ],
+  },
+  {
+    name: 'crear-ficha',
+    resolver: createCharacter,
+    options: [],
+  },
+  {
+    name: 'v',
+    resolver: (_, context, messenger) => {
+      messenger.send(
+        'changelog',
+        undefined,
+        context
+      )
+    },
+  },
+  // Clasicos del mazmorreo
+  {
+    name: 'cdm',
+    resolver: cdmCreateACharacter,
+    options: [],
+  },
+]
+
+// test  command
+if (process.env.NODE_ENV === 'development')
+  commands.push({
+    name: 'ts',
+    resolver: testCommand,
+    options: [],
+  })
 
 export default new CommandManager(commands, {
   prefix: '.',
