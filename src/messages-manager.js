4a24c023daaa896e81f11f4d020b792d5073c47c
Parrish <paarriisshh@yahoo.com.mx>
2020-06-14 00:58:38 -0500

    refactor(messageManager): Move all messages to message-manager module

--- src/messages-manager.js
+++ src/messages-manager.js
@@ -1,6 +1,112 @@
 import MessageManager from './message-manager'
-import messages from './messages'
 import bot from './bot'
+import roll from './messages/roll'
+import abilityRoll from './messages/ability-roll'
+import changelog from './messages/changelog'
+import {
+  notNumberVariable,
+  variableSaved,
+  variableNotFound,
+  variable,
+  notFoundVariables,
+  allVariables,
+  deletedVariable,
+} from './messages/variables'
+import issue from './messages/issue'
+import {
+  table,
+  allTables,
+  tableNotFound,
+  tableNotExist,
+} from './messages/tables'
+import {
+  sheetNotExist,
+  sheetList,
+  sheetChange,
+  sheetAvatarChange,
+  sheetProccessing,
+  sheetProccesed,
+  sheetBadAttachment,
+  sheetDeleted,
+} from './messages/sheet'
+import cdmCharacter from './clasicos-del-mazmorreo/messages/character'
+import {
+  tldr,
+  tldrAll,
+  tldrNitFound,
+  tldrNotExist,
+} from './messages/tldr'
+import rollGeneratorType from './messages/roll-generator-type'
+import {
+  lifeActual,
+  lifeIsNull,
+  lifeSetted,
+  lifeNotPoints,
+} from './messages/life-points'
+
+import help from './messages/help'
+
+const messages = [
+  {
+    name: 'raw',
+    resolver: ({ text = '' }, { channel }) => {
+      if (text)
+        return channel.send(text, {
+          split: true,
+        })
+      return false
+    },
+  },
+  {
+    name: 'non setted variables',
+    resolver: (
+      nonSettedVariables,
+      { channel }
+    ) => {
+      channel.send(`No he podido encontrar esta${
+        nonSettedVariables.length > 1 ? 's' : ''
+      } habilidad${
+        nonSettedVariables.length > 1 ? 'es' : ''
+      }: ${nonSettedVariables}
+Usa \`.gv\` para ver todas tus habilidades guardadas.
+`)
+    },
+  },
+  roll,
+  abilityRoll,
+  notNumberVariable,
+  variableNotFound,
+  variableSaved,
+  variable,
+  notFoundVariables,
+  allVariables,
+  deletedVariable,
+  issue,
+  table,
+  allTables,
+  tableNotExist,
+  tableNotFound,
+  sheetNotExist,
+  sheetList,
+  sheetChange,
+  sheetAvatarChange,
+  sheetProccessing,
+  sheetProccesed,
+  sheetBadAttachment,
+  sheetDeleted,
+  cdmCharacter,
+  tldr,
+  tldrAll,
+  tldrNitFound,
+  tldrNotExist,
+  rollGeneratorType,
+  lifeActual,
+  lifeIsNull,
+  lifeSetted,
+  lifeNotPoints,
+  help,
+  changelog,
+]
 
 export default new MessageManager(
   messages,
