07e125e1a14a94425a4242c71f574bd9a07de97f
Parrish Sanchez <ppaarriisshh@gmailcom>
2021-08-07 00:45:01 -0500

    fix(bot): The issue command now uses the new discord api

--- src/messages/issue.js
+++ src/messages/issue.js
@@ -2,9 +2,11 @@ export default {
   name: 'to issue channel',
   resolver: (message, _, client) => {
     if (!message) return false
-    client.guilds
-      .find(x => x.id === '560194891942985760')
-      .channels.find(x => x.name === 'issues')
+    client.guilds.cache
+      .find((x) => x.id === '560194891942985760')
+      .channels.cache.find(
+        (x) => x.name === 'issues'
+      )
       .send(message)
     return true
   },
