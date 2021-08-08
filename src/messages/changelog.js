7618c5a66afc3fcfa93ad17e795d1b08f35da344
Parrish Sanchez <ppaarriisshh@gmailcom>
2021-08-07 20:08:31 -0500

    fix(commands): Update find channel to new api

--- src/messages/changelog.js
+++ src/messages/changelog.js
@@ -1,13 +1,13 @@
 export default {
   name: 'changelog',
   resolver: async (_, { channel }, client) => {
-    const updatesChannel = client.guilds
-                                 .find((x) => x.id === '560194891942985760')
-                                 .channels.find((x) => x.name === 'updates')
+    const updatesChannel = client.guilds.cache
+      .find((x) => x.id === '560194891942985760')
+      .channels.find((x) => x.name === 'updates')
     const { lastMessageID } = updatesChannel
     const lastMessage =
       await updatesChannel.fetchMessage(
-        lastMessageID,
+        lastMessageID
       )
 
     return channel.send(lastMessage.content)
